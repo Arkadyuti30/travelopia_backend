1c69032d9118ed262e4b8f82a1b7f5297ebbc378
arkadyuti30 <arkadyutipaul@gmail.com>
2023-05-31 09:35:16 +0530

    changes related to apis

--- index.js
+++ index.js
@@ -1,21 +1,94 @@
+///////////////////////////////////////////////
+/****** BASIC SETUP RELATED CODE HERE *******/
+/////////////////////////////////////////////
+
 const express = require('express')
 const app = express()
 const PORT = 8080
 
-app.use(express.json()) // middleware
+// Using middleware to parse json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+
+
+
+////////////////////////////////////////////
+/****** DATABASE RELATED CODE HERE *******/
+//////////////////////////////////////////
+
+// mongodb database connection using mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://arkadyutipaul:p%40ssw0rd@travel-app-cluster0.y33ulkg.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true});
+
+// creating model for storing data
+const TravelFormSchema = new mongoose.Schema({
+	name: String,
+	email: String,
+	place: String,
+	travellers: Number,
+	budget: String,
+	date: String
+})
+
+const Form = mongoose.model('Form', TravelFormSchema)
+
+// DATABASE RELATED CODE ENDS HERE //
+
+
+
+
+// To check that the app is live
 app.listen(
 	PORT,
 	() => console.log(`App is live on ${PORT}`)
 )
 
-app.get('/all/form/data', (req, res) => {
-	res.status(200).send({
-		name: "Yun",
-		email: "yyy@g.com"
+
+
+
+///////////////////////////////////////
+/****** API RELATED CODE HERE *******/
+/////////////////////////////////////
+
+// GET API - gets all the travel forms data
+app.get('/forms', (req, res) => {
+	Form.find().then((foundForms) => {
+		res.status(200).send(foundForms)
+		console.log(foundForms)
+	}).catch((err) => {
+		console.log(`Error occured in GET /forms api: ${JSON.stringify(err)}`)
 	})
 })
 
-app.post('/submit/form', (req, res) => {
 
+// POST API - posts data of a single travel form to store in the db (mongodb atlas)
+app.post('/submit/form', (req, res) => {
+	let formData = req.body
+
+	if(!formData || !formData.name || !formData.email || !formData.place || !formData.travellers || !formData.budget) {
+		console.log(`Missing data from request body. req body: ${JSON.stringify(formData)}`)
+		res.status(400).send({ message: `Missing data from request body. req body: ${JSON.stringify(formData)}` })
+	}
+
+	// adding date to form data
+	let date = new Date() // creating date object
+	let finalDate = ""+date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear() // formatting the date in DD/MM/YYYY format
+	formData.date = finalDate // added date to form data
+
+	const newFormData = new Form({
+		name: formData.name,
+		email: formData.email,
+		place: formData.place,
+		travellers: formData.travellers,
+		budget: formData.budget,
+		date: formData.date
+	});
+	newFormData.save().then(() => {
+		res.status(200).send({message: 'Successfully saved new form data to db!'})
+		console.log('Successfully saved new form data to db!')
+	}).catch((err) => {
+		res.send(`Error occured ${JSON.stringify(err)}`)
+		console.log(`Error occured in POST /submit/form api: ${JSON.stringify(err)}`)
+	})
 })
